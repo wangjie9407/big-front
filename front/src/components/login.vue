e3c2c258fa3daa91e45082d5ff054c461fe03b05
wj <1219363140@qq.com>
2021-03-26 22:12:49 +0800

    引入layui样式库

---
+++ front/src/components/login.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        name:'login',
+        render(){
+            return (
+                <div className="login-box">
+                    <form class="layui-form layui-form-pane" action="">
+                      <div class="layui-form-item" pane>
+                        <label class="layui-form-label">单选框</label>
+                        <div class="layui-input-block">
+                          <input type="radio" name="sex" value="男" title="男"/>
+                          <input type="radio" name="sex" value="女" title="女" checked/>
+                        </div>
+                      </div>
+                    </form>
+                </div>
+            )
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
